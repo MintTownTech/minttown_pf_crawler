2a42127ff776d52cbecb36941f7f48b8a8c48536
Brian.Dang <hung.viet@minttown.jp>
2025-01-20 14:08:36 +0700

    Init tmp function for test

--- src/freecash/freecash.utils.ts
+++
@@ -1,7 +0,0 @@
-import Decimal from 'decimal.js';
-
-export const FREECASH_COIN_CONVERSION_RATE = 1000;
-
-export const toDecimalFixed = (value: number | string | Decimal) => {
-    return new Decimal(value).toDecimalPlaces(10);
-};
--- src/prisma.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from 'minttown_pf_community/prisma/client';
-
-export const prisma = new PrismaClient();
