e1e9efd41dea7ac4a0cc795eebc3d2c2a9636346
Brian.Dang <hung.viet@minttown.jp>
2025-01-21 09:14:46 +0700

    Init cicd test

---
+++ terraform/variable.tf
@@ -0,0 +1,22 @@
+variable "organization_id" {
+    description = "Session ID for Freecash API"
+    type        = string
+}
+variable "bucket_name" {
+    description = "AWS Account ID for the Crawler Account"
+    type        = string
+}
+variable "TFC_AWS_RUN_ROLE_ARN" {
+    description = "AWS Account ID for the DEV Account"
+    type        = string
+}
+
+variable "freecash_session_id" {
+    description = "AWS Account ID for the DEV Account"
+    type        = string
+}
+
+variable "crawler_aws_account_id" {
+    description = "AWS Account ID for the Crawler Account"
+    type        = string
+}
