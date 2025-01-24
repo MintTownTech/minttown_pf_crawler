bcc26b0b9edcc612ea73d47b9982fb0073140497
Brian.Dang <hung.viet@minttown.jp>
2025-01-24 21:15:52 +0700

    Updated function resource

--- terraform/crawler/main.tf
+++ terraform/crawler/main.tf
@@ -110,11 +110,10 @@ resource "aws_s3_bucket_policy" "cross_crawler_aws_account_access" {
         Effect = "Allow"
         Principal = {
           AWS = [
-            "arn:aws:iam::309217545237:root",
-            "arn:aws:iam::340258365836:role/updated-crawler-function-role-dev",
-            "arn:aws:iam::309217545237:role/updated-crawler-function-role-sb",
-            "arn:aws:iam::340258365836:role/updated-crawler-function-role-game",
-            "arn:aws:iam::045372454064:role/updated-crawler-function-role-prod"
+              "arn:aws:iam::340258365836:role/updated-crawler-function-role-sb",
+              "arn:aws:iam::340258365836:role/updated-crawler-function-role-dev",
+              "arn:aws:iam::309217545237:root",
+              "arn:aws:iam::340258365836:root"
           ]
         }
         Action = [
@@ -122,7 +121,7 @@ resource "aws_s3_bucket_policy" "cross_crawler_aws_account_access" {
             "s3:GetObjectVersion",
             "s3:GetObjectAcl"
         ]
-        Resource = ["${aws_s3_bucket.data_bucket.arn}/*"]
+        Resource = "${aws_s3_bucket.data_bucket.arn}/*"
       }
     ]
   })
