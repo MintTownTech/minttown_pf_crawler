fb32b14f459dbe2036d3a708b0b808ea6622d6ee
Brian.Dang <hung.viet@minttown.jp>
2025-01-24 20:51:44 +0700

    Updated function resource

--- terraform/crawler/main.tf
+++ terraform/crawler/main.tf
@@ -111,7 +111,10 @@ resource "aws_s3_bucket_policy" "cross_crawler_aws_account_access" {
         Principal = {
           AWS = [
             "arn:aws:iam::309217545237:root",
-            "arn:aws:iam::309217545237:role/updated-crawler-function-role-*",
+            "arn:aws:iam::309217545237:role/updated-crawler-function-role-dev",
+            "arn:aws:iam::309217545237:role/updated-crawler-function-role-sb",
+            "arn:aws:iam::309217545237:role/updated-crawler-function-role-game",
+            "arn:aws:iam::309217545237:role/updated-crawler-function-role-prod",
           ]
         }
         Action = [
